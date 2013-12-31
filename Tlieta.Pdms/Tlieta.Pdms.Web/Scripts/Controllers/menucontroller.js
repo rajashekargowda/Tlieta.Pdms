47e03249665c07db2b95cd6642aa3c03217adcaa
sachinkumark <sachinkumark@outlook.com>
2014-01-01 04:30:17 +0530

    CSS

--- Tlieta.Pdms/Tlieta.Pdms.Web/Scripts/Controllers/menucontroller.js
+++ Tlieta.Pdms/Tlieta.Pdms.Web/Scripts/Controllers/menucontroller.js
@@ -3,5 +3,7 @@ function menucontroller($scope) {
     $scope.redirect = function (menu) {
         if (menu == 'contact')
             location.href = '/Contact/Manage/';
+        else if (menu == 'addpatient')
+            location.href = '/Patient/Create/';
     };
 }
