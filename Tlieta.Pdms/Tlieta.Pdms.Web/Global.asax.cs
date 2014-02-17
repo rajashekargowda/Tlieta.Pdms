9152c7d701f4aff8cc81f89543203ff32cf59164
sachinkumark <sachinkumark@outlook.com>
2014-02-17 15:06:25 +0530

    test

--- Tlieta.Pdms/Tlieta.Pdms.Web/Global.asax.cs
+++ Tlieta.Pdms/Tlieta.Pdms.Web/Global.asax.cs
@@ -18,8 +18,8 @@ namespace Tlieta.Pdms.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new SessionOutHandler());
-            filters.Add(new System.Web.Mvc.AuthorizeAttribute());
+            //filters.Add(new SessionOutHandler());
+            //filters.Add(new System.Web.Mvc.AuthorizeAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
@@ -50,67 +50,67 @@ namespace Tlieta.Pdms.Web
         protected void Application_Error(object sender, EventArgs e)
         {
 
-            var httpContext = ((MvcApplication)sender).Context;
-            var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
-            Logging.LogError(httpContext.Error);
-            try
-            {
-                var currentController = " ";
-                var currentAction = " ";
+            //var httpContext = ((MvcApplication)sender).Context;
+            //var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+            //Logging.LogError(httpContext.Error);
+            //try
+            //{
+            //    var currentController = " ";
+            //    var currentAction = " ";
 
-                if (currentRouteData != null)
-                {
-                    if (currentRouteData.Values["controller"] != null && !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
-                    {
-                        currentController = currentRouteData.Values["controller"].ToString();
-                    }
+            //    if (currentRouteData != null)
+            //    {
+            //        if (currentRouteData.Values["controller"] != null && !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+            //        {
+            //            currentController = currentRouteData.Values["controller"].ToString();
+            //        }
 
-                    if (currentRouteData.Values["action"] != null && !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
-                    {
-                        currentAction = currentRouteData.Values["action"].ToString();
-                    }
-                }
+            //        if (currentRouteData.Values["action"] != null && !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+            //        {
+            //            currentAction = currentRouteData.Values["action"].ToString();
+            //        }
+            //    }
 
-                //if (currentController.ToLower() != "home" && currentController.ToLower() != "favicon.ico")
-                if (currentController.ToLower() != "favicon.ico")
-                {
-                    var ex = Server.GetLastError();
-                    var controller = new ErrorController();
-                    var routeData = new RouteData();
-                    var action = "Error";
+            //    //if (currentController.ToLower() != "home" && currentController.ToLower() != "favicon.ico")
+            //    if (currentController.ToLower() != "favicon.ico")
+            //    {
+            //        var ex = Server.GetLastError();
+            //        var controller = new ErrorController();
+            //        var routeData = new RouteData();
+            //        var action = "Error";
 
-                    if (ex is HttpException)
-                    {
-                        var httpEx = ex as HttpException;
+            //        if (ex is HttpException)
+            //        {
+            //            var httpEx = ex as HttpException;
 
-                        switch (httpEx.GetHttpCode())
-                        {
-                            case 404:
-                                action = "SessionTimeout";
-                                routeData.Values["controller"] = "Home";
-                                break;
+            //            switch (httpEx.GetHttpCode())
+            //            {
+            //                case 404:
+            //                    action = "SessionTimeout";
+            //                    routeData.Values["controller"] = "Home";
+            //                    break;
 
-                            case 401:
-                                action = "Error";
-                                routeData.Values["controller"] = "Error";
-                                break;
-                        }
-                    }
+            //                case 401:
+            //                    action = "Error";
+            //                    routeData.Values["controller"] = "Error";
+            //                    break;
+            //            }
+            //        }
 
-                    httpContext.ClearError();
-                    httpContext.Response.Clear();
-                    httpContext.Response.StatusCode = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
-                    httpContext.Response.TrySkipIisCustomErrors = true;
+            //        httpContext.ClearError();
+            //        httpContext.Response.Clear();
+            //        httpContext.Response.StatusCode = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
+            //        httpContext.Response.TrySkipIisCustomErrors = true;
 
-                    routeData.Values["action"] = action;
+            //        routeData.Values["action"] = action;
 
-                    controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
-                    ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
-                }
-            }
-            catch
-            {
-            }
+            //        controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+            //        ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            //    }
+            //}
+            //catch
+            //{
+            //}
         }
     }
 }
